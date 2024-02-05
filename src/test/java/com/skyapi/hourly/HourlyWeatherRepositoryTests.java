b168489f294921bdac634557a923a308dcf88060
Mohammad-Rnjbr <mohammadranjbar.mr81@gmail.com>
2024-02-05 17:38:07 +0330

    code testDelete in HourlyWeatherRepositoryTests class

--- src/test/java/com/skyapi/hourly/HourlyWeatherRepositoryTests.java
+++ src/test/java/com/skyapi/hourly/HourlyWeatherRepositoryTests.java
@@ -1,6 +1,7 @@
 package com.skyapi.hourly;
 
 import com.skyapi.weatherapicommon.model.HourlyWeather;
+import com.skyapi.weatherapicommon.model.HourlyWeatherId;
 import com.skyapi.weatherapicommon.model.Location;
 import com.skyapi.weatherapiservice.repository.HourlyWeatherRepository;
 import org.assertj.core.api.Assertions;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.annotation.Rollback;
+
+import java.util.Optional;
 
 @DataJpaTest
 @Rollback(value = false)
@@ -37,6 +40,16 @@ public class HourlyWeatherRepositoryTests {
         HourlyWeather hourlyWeather = hourlyWeatherRepository.save(forecast);
         Assertions.assertThat(hourlyWeather.getId().getLocation().getCode()).isEqualTo(location_code);
         Assertions.assertThat(hourlyWeather.getId().getHourOfDay()).isEqualTo(hourOfDay);
+    }
+
+    @Test
+    public void testDelete(){
+        Location location = new Location().code("DEHLI_IN");
+        HourlyWeatherId id = new HourlyWeatherId(13,location);
+        hourlyWeatherRepository.deleteById(id);
+
+        Optional<HourlyWeather> result = hourlyWeatherRepository.findById(id);
+        Assertions.assertThat(result).isNotPresent();
     }
 
 }
